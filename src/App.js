b4f4a193e945e5b5383f14a54bea6192fd657aa6
serge mirouse <sergemirouse@MBP-de-serge.lan>
2023-01-30 15:21:43 +0100

    step 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div>
+        <h1>Lorem</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          porttitor aliquet risus, sed venenatis dui rutrum sed. Praesent
+          ultrices feugiat felis ac volutpat. Etiam id rhoncus dui. Praesent
+          porttitor massa vel massa efficitur, ac aliquet magna hendrerit. Cras
+          convallis sodales eros a molestie. Nullam ipsum tellus, egestas quis
+          dolor facilisis, rutrum gravida purus. Morbi ut sodales ligula. Sed
+          lacus risus, sagittis ac lobortis eget, semper ornare orci. Aenean
+          vehicula lobortis nisi a laoreet. Aliquam quis dictum augue. Cras sit
+          amet lacinia sem. Pellentesque porta elit at purus sollicitudin, at
+          tempor tortor vulputate.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
